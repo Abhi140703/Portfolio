18a639003f1073631b239e995461aea3a94f1230
Abhishek DungDung <abhishekdungdung140703@gmail.com>
2026-01-30 21:40:40 +0530

    Update API URLs for production

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import api from "../api/api";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
--- frontend/src/components/BlogDetails.jsx
+++ frontend/src/components/BlogDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import api from "../api/api";
 
 export default function BlogDetails() {
   const { slug } = useParams();
