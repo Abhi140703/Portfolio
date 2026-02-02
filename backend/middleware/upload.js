8c994c846ab1d477da9dc769e1dd9be01f2782c8
Abhishek DungDung <abhishekdungdung140703@gmail.com>
2026-02-02 08:29:52 +0530

    Update API URLs for production

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,15 +1,15 @@
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
+const { storage } = require("multer-storage-cloudinary");
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "blogs",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
-  },
+const upload = multer({
+  storage: storage({
+    cloudinary,
+    params: {
+      folder: "portfolio",
+      allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    },
+  }),
 });
-
-const upload = multer({ storage });
 
 module.exports = upload;
