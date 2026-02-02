a4259fbc66b37176091d28d8c4c588505d0ace2d
Abhishek DungDung <abhishekdungdung140703@gmail.com>
2026-02-02 09:31:27 +0530

    add register route for admin

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,37 +6,4 @@ const { login } = require("../controllers/authController");
 
 router.post("/login", login);
 
-// ✅ TEMPORARY REGISTER ROUTE
-router.post("/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = new User({
-      username,
-      password: hashedPassword,
-    });
-
-    await user.save();
-    res.json({ message: "User created" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-router.put("/reset-password", async (req, res) => {
-  const bcrypt = require("bcryptjs");
-  const User = require("../models/User");
-
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-  await User.updateOne(
-    { username: req.body.username },
-    { $set: { password: hashedPassword } }
-  );
-
-  res.json({ message: "Password reset successful" });
-});
-
-
 module.exports = router;
