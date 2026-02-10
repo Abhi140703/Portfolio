cda15118d8ba822eff63740e2cb67f7d5e308c97
Abhishek Dungdung <abhishekdungdung140703@gmail.com>
2026-02-10 08:50:11 +0530

    temp route to create admin user

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,22 +6,4 @@ const { login } = require("../controllers/authController");
 
 router.post("/login", login);
 
-/* TEMP: create admin ONCE */
-router.post("/create-admin", async (req, res) => {
-  const exists = await User.findOne({ username: "admin" });
-  if (exists) {
-    return res.status(400).json({ message: "Admin already exists" });
-  }
-
-  const hashedPassword = await bcrypt.hash("admin123", 10);
-
-  const user = new User({
-    username: "admin",
-    password: hashedPassword,
-  });
-
-  await user.save();
-  res.json({ message: "Admin created" });
-});
-
 module.exports = router;
