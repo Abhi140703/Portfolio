7fba02c0357b5f0407f2c604d275de5a22189a1d
Abhishek Dungdung <abhishekdungdung140703@gmail.com>
2026-02-10 07:26:58 +0530

    update server config

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const auth = require("./middleware/authMiddleware");
+const auth = require("./middleware/auth");
 
 const app = express();
 
